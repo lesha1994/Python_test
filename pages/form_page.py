8e539d5f9fd1095830e7c6f4da0c66bb4219e6c6
opliukhin <dp091294paa@gmail.com>
2023-05-23 22:11:58 +0300

    problem with locator

---
+++ pages/form_page.py
@@ -0,0 +1,33 @@
+import time
+
+from selenium.webdriver import Keys
+
+from pages.base_page import BasePage
+from locators.form_page_locators import FormPageLocators as Locators
+
+class FormPage(BasePage):
+
+    def fill_fields_and_submit(self):
+        first_name = 'Oleksii'
+        last_name = 'Pliukhin'
+        email = 'test@gmail.com'
+        self.element_is_visible(Locators.FIRST_NAME).send_keys(first_name)
+        self.element_is_visible(Locators.LAST_NAME).send_keys(last_name)
+        self.element_is_visible(Locators.EMAIL).send_keys(email)
+        self.element_is_visible(Locators.GENDER).click()
+        self.element_is_visible(Locators.MOBILE).send_keys('0931204799')
+        subject =  self.element_is_visible(Locators.SUBJECT)
+        subject.send_keys('English')
+        subject.send_keys(Keys.RETURN)
+        self.element_is_visible(Locators.HOBBIES).click()
+        self.element_is_visible(Locators.FILE_INPUT).send_keys(r'C:\Users\opliukhin\PycharmProjects\pythonProject\text.txt')
+        self.element_is_visible(Locators.CURRENT_ADDRESS).send_keys('Dnipro City')
+        self.element_is_visible(Locators.SUBMIT).click()
+        return first_name, last_name, email
+
+    def form_result(self):
+        result_list = self.element_is_visible(Locators.RESULT_TABLE)
+        result_text = []
+        for i in result_list:
+            result_text.append(i.text)
+        return result_text
